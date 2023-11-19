485756683c0a1488d9c898fadeed55b9479f0026
Breiner Munoz <breiner.munoz.fallas@est.una.ac.cr>
2023-11-18 21:36:28 -0600

    Arreglos al menu y puntos optenidos

--- Proyecto.asm
+++ Proyecto.asm
@@ -5,10 +5,11 @@
     HANDLE DW ?
     Caracter DB ?
     ROW DB 00
+  ;Datos para mostrar  
     Mensaje_Menu DB "Bienvenido al Menu", 10, 13, "$"
     Mensaje_Seleccion DB "Digite el numero para seleccionar una opcion", 10, 13, "$"
     Mensaje_Establecer DB "1-Establecer nivel y nombre del jugador", 10, 13, "$"
-    Mensaje_Nombre DB "Al finalizar precione enter", 10,13,"Ingrese su nombre (Con un maximo de 20 caracteres)", 10, 13, "$"
+    Mensaje_Nombre DB "Al finalizar precione enter", 10,13, 10, 13,"Ingrese su nombre (Con un maximo de 20 caracteres)", 10, 13, "$"
     Mensaje_Nivel DB 10, 13, "Ingrese el nivel de dificultad", 10,13 ,"(Con un maximo de 2 caracteres, solo se tomara en cuenta numeros)", 10, 13, "$"
     Mensaje_Escenario DB "2-Seleccione el escenario", 10, 13, "$"
     Mensaje_Iniciar DB "3-Iniciar juego", 10, 13, "$"
@@ -17,11 +18,13 @@
     Nombre DB 20 dup('$')
     Nivel DB 3 dup('$')
     Archivo_Acerca DB "Acerca.txt", 0
-    Print_Nombre DB "Nombre del Jugador: ", 10, 13, "$"
-    Print_Nivel DB 10, 13,"Nivel de dificultad: ", 10, 13, "$"
-    Print_Nivel_Actual DB 3 dup("$")
-    suma DB 3 DUP('$')
-    ;;;;;
+    Print_Nombre DB "Nombre del Jugador: $"
+    Print_Nivel DB "Nivel de dificultad: $"
+    Print_Puntos_Obtenidos DB "Puntos obtenidos: $"
+    Print_Dato_Actual DB 6 dup("$")
+    Print_Instrucciones DB "Instrucciones: ", 10, 13, "Para moverse arriba presione la tecla 'w' o la flecha hacia arriba", 10, 13, "Para moverse abajo presione la tecla 's' o la flecha hacia abajo", 10, 13, "Para pausar el juego presione la tecla 'p'", 10, 13, "Para subir de nivel presione la tecla 'n'", 10, 13, "Para salir del juego presione la tecla 'e'", 10, 13, "$"
+  ;variables para el juego
+    Max_Mostrar_Datos dw 0
     cuadro_Personaje DB 219
     filaLimiteMin DB 10
     filaLimiteMax DB 14
@@ -29,6 +32,8 @@
     columna DB 20
     counter DB 10
     sumar DB 10
+    contar_ciclos db 0
+    puntos_Obtenidos dw 1
     posicion macro x, y 
         mov ah, 02h
         mov bh, 00h
@@ -51,6 +56,10 @@
     contador_loop dw 0
     Nivel_actual dw 1
     table_Nivel DW 0, 14, 30, 40, 55, 70, 85, 100, 115, 130, 145, 165, 180, 200, 230
+    primer_Segundo db 0
+    segundo_Se db 0
+    aux_primer_se db 0
+    resultado_resta db 0
 
 .CODE
 MOV AX, @DATA
@@ -63,24 +72,31 @@ Inicio:
     CALL Seleccion
     JMP Inicio
 Mostrar_Inicio PROC NEAR
+    posicion 4, 30
     mov AH, 09h 
 	lea dx, Mensaje_Menu
 	int 21h
+    posicion 6, 18
     mov AH, 09h
     lea dx, Mensaje_Seleccion
     int 21h
+    posicion 8, 20
     mov AH, 09h
     lea dx, Mensaje_Establecer
     int 21h
+    posicion 10, 26
     mov AH, 09h
     lea dx, Mensaje_Escenario
     int 21h
+    posicion 12, 31
     mov AH, 09h
     lea dx, Mensaje_Iniciar
     int 21h
+    posicion 14, 33
     mov AH, 09h
     lea dx, Mensaje_Acerca
     int 21h
+    posicion 16, 35
     mov AH, 09h
     lea dx, Mensaje_Fin
     int 21h
@@ -88,6 +104,7 @@ Mostrar_Inicio PROC NEAR
     Mostrar_Inicio ENDP
 
 Seleccion PROC NEAR
+    posicion 18, 36
     mov ah, 01h
     int 21h
     cmp al, '1'
@@ -122,7 +139,7 @@ Establecer PROC NEAR
     CALL limpiar_datos
     CALL Limpia
     CALL Colocar_Cursor
-
+    posicion 1,30
     mov ah, 09h
     lea dx, Mensaje_Nombre
     int 21h
@@ -138,7 +155,7 @@ Establecer PROC NEAR
         jmp Guardar
     Seguir2:
     
-
+    posicion 5, 0
      mov ah, 09h
     lea dx, Mensaje_Nivel
     int 21h
@@ -183,15 +200,21 @@ Establecer PROC NEAR
         jl Nivel_Menor
         cmp ax, 15
         jg Nivel_Mayor
+        DEC ax
         mov [Nivel_actual], ax  ; Almacena el valor numérico en Nivel_actual
+        CALL Actualizar_Nivel
         ret
     Nivel_Menor:
         mov ax, 1
-        mov [Nivel_actual], ax
+        DEC ax
+        mov [Nivel_actual], ax  ; Almacena el valor numérico en Nivel_actual
+        CALL Actualizar_Nivel
         ret
     Nivel_Mayor:
         mov ax, 15
-        mov [Nivel_actual], ax
+        DEC ax
+        mov [Nivel_actual], ax  ; Almacena el valor numérico en Nivel_actual
+        CALL Actualizar_Nivel
         ret
 
     limpiar_datos PROC NEAR
@@ -212,32 +235,32 @@ Establecer PROC NEAR
     
      xor bx, bx
     RET
-
+    
     limpiar_datos ENDP
     Establecer ENDP
 
 
 Escenario PROC NEAR
-    
+    mov AH, 2ch
+    int 21H
+    mov primer_Segundo, dh
+    call niveles_auto
     RET
     Escenario ENDP
 Iniciar PROC NEAR
-    CALL Actualizar_Nivel
+    
+    mov AH, 2ch
+    int 21H
+    mov primer_Segundo, dh
     ciclo:
+    mov [segundo_Se], 0
     CALL Mostrar_Datos
-    CALL Dibujar_Cuadro
-    dec columna2
-    call Mostrar_Obstaculo
-    call Mostrar_Avatar
     call Bucle_Delay
-   mov ah, 01h
-   int 16h
-   ; Se preciona una tecla?
-   jz sin_tecla_presionada
-    ; Si se preciona una tecla, entonces se lee el codigo de la tecla
+      mov ah, 01h
+   int 16h  ; Se preciona una tecla?
+   jz sin_tecla_presionada; Si se preciona una tecla, entonces se lee el codigo de la tecla 
    mov ah, 00h
    int 16h
-   ; Mover el cuadro segun las teclas
    cmp al, 'w'
    je mover_arriba
    cmp al, 's'
@@ -253,58 +276,62 @@ Iniciar PROC NEAR
    cmp al, 'n'
    je subir_nivel
    sin_tecla_presionada:
+    cmp contar_ciclos, 10
+    jge aumentar_nivel
+    inc contar_ciclos
    jmp ciclo
-
+    aumentar_nivel:
+        mov contar_ciclos, 0
+        call niveles_auto
+        jmp ciclo
     pausa_Llamar:
-        call pausa
-        jmp ciclo
-    subir_nivel:
-        mov ax, [Nivel_actual]
-        cmp al, 15
-        je ciclo
-        inc al
-        mov Nivel_actual, ax
-        call Actualizar_Nivel
-        jmp ciclo
-    mover_arriba:
-        dec fila
-        mov al, filaLimiteMin
-        cmp fila, al
-        jl fila_Limite_Min
-        JMP ciclo
-    mover_abajo:
-        inc fila
-        mov al, filaLimiteMax
-        cmp fila, al
-        jge fila_Limite_Max
-        JMP ciclo
-    fila_Limite_Min:
-        mov al, filaLimiteMin
-        mov fila,al
-        JMP ciclo
-    fila_Limite_Max:
-        mov al, filaLimiteMax
-        mov fila,al
-        JMP ciclo
-
-    pausa proc near
-        CALL Limpia
-        CALL Dibujar_Cuadro
-        call Mostrar_Obstaculo
-        call Mostrar_Avatar
-            mov ah, 01h
-            int 21h
-            cmp al, 'p'
-            je salir_Pausa
-            cmp al, 'e'
-            je salir_Iniciar
-            JMP pausa
-        salir_Pausa:
-        RET
+            call pausa
+            jmp ciclo
+        subir_nivel:
+            mov ax, [Nivel_actual]
+            cmp al, 15
+            je ciclo
+            call Actualizar_Nivel
+            jmp ciclo
+        mover_arriba:
+            dec fila
+            mov al, filaLimiteMin
+            cmp fila, al
+            jl fila_Limite_Min
+            JMP ciclo
+        mover_abajo:
+            inc fila
+            mov al, filaLimiteMax
+            cmp fila, al
+            jge fila_Limite_Max
+            JMP ciclo
+        fila_Limite_Min:
+            mov al, filaLimiteMin
+            mov fila,al
+            JMP ciclo
+        fila_Limite_Max:
+            mov al, filaLimiteMax
+            mov fila,al
+            JMP ciclo
+        pausa proc near
+            CALL Limpia
+            CALL Mostrar_Datos
+            CALL Dibujar_Cuadro
+            call Mostrar_Obstaculo
+            call Mostrar_Avatar            
+                mov ah, 01h
+                int 21h
+                cmp al, 'p'
+                je salir_Pausa
+                cmp al, 'e'
+                je salir_Iniciar
+                JMP pausa
+            salir_Pausa:
+            RET
         pausa endp
-
     salir_Iniciar:
     mov [Nivel_actual], 1
+    mov [puntos_Obtenidos], 1
     RET
     Bucle_Delay PROC NEAR
 
@@ -323,25 +350,18 @@ Iniciar PROC NEAR
         Bucle_Delay ENDP
     
     Actualizar_Nivel PROC NEAR
-        cmp [nivel_actual], 1
-        je nivel1
+        inc [Nivel_actual]
+        mov [segundo_Se], 0
         mov ax, 250
         mov  [final_loop], ax
-        dec [nivel_actual]
-        mov ax, [nivel_actual]
+        MOV AX, Nivel_actual      ; Cargar el valor de 'count' en AX
+        dec ax
         mov bl, al            ; Mueve el valor de al a bl
         shl bl, 1             ; Multiplica bl por 2 
         mov ax, [table_Nivel + bx]  ; Usa bx como índice
         mov cx, [final_loop]
         sub cx, ax
         mov [final_loop], cx
-        inc  [nivel_actual]  ; Restore nivel_actual
-        ret
-
-        nivel1:
-        mov  [final_loop], 0
-        mov ax, 126
-        mov  [final_loop], ax
         ret
         Actualizar_Nivel ENDP
 
@@ -405,84 +425,154 @@ Iniciar PROC NEAR
         RET
         Mostrar_Avatar ENDP
     Mostrar_Datos PROC NEAR
-    CALL Limpia  
-    CALL Colocar_Cursor
-    mov ah, 09h
-    lea dx, Print_Nombre
-    int 21h
-    mov ah, 09h
-    lea dx, Nombre
-    int 21h
-    mov ah, 09h
-    lea dx, Print_Nivel
-    int 21h
-    Call LlenarCadena
-    ;;;;;;;;;;;;;;;;
-    MOV AX, Nivel_actual      ; Cargar el valor de 'count' en AX
-	MOV DX, OFFSET suma
-    CALL INT_TO_STR   ; Llama a una funcion para convertir el numero en una cadena
-    MOV AH, 09h        ; Servicio para imprimir una cadena
-    MOV DX, OFFSET suma ; DX apunta a la cadena
-    INT 21h     
-  
-    RET
+        CALL Limpia 
+        posicion 19, 0
+        MOV AH, 09h       ; Función 09h - Imprimir cadena
+        LEA DX, Print_Instrucciones ; Dirección de la cadena
+        INT 21h
+        posicion 1, 0
+        mov ah, 09h
+        lea dx, Print_Nombre
+        int 21h
+        posicion 1, 21
+        mov ah, 09h
+        lea dx, Nombre
+        int 21h
 
-
-    INT_TO_STR PROC
-    ; Convierte el numero en AX en una cadena y almacena la cadena en 'suma'
-    ; Esta funcion asume que el numero es menor o igual a 65535 (maximo valor de un registro AX)
-
-    PUSH AX
-    PUSH BX
-    PUSH CX
-    PUSH DX
-    
-    MOV CX, 10          ; Divisor para convertir en decimal
-    XOR BX, BX          ; BX se usa para mantener el índice en 'suma'
-    MOV DI, OFFSET suma + 2   ; Puntero al final de la cadena
-    MOV BYTE PTR [DI], '$' ; Fin de cadena nul-terminada
-
-    ConvertLoop:
-        XOR DX, DX
-        DIV CX              ; Dividir AX por 10, resultado en AX, residuo en DX
-        ADD DL, '0'         ; Convertir el residuo en caracter
-        DEC DI
-        MOV [DI], DL        ; Almacenar el caracter en 'suma'
-        INC BX
-        TEST AX, AX
-    JNZ ConvertLoop
-
-    LEA SI, [DI]        ; SI apunta al inicio de la cadena (justo después de '$')
-    MOV DI, OFFSET suma       ; DI apunta al destino de la cadena
-    MOV CX, BX          ; CX contiene la longitud de la cadena
-
-    CopyLoop:
-        LODSB               ; Cargar un byte desde SI
-        STOSB               ; Almacenar el byte en DI
-        LOOP CopyLoop
-
-    POP DX
-    POP CX
-    POP BX
-    POP AX
-
-    RET
-    INT_TO_STR ENDP
-
-    LlenarCadena proc near
-    mov si, 00h
-    LlenarCadena1:
-    mov al, '$'
-    repne scasb ; Busca el caracter '$' en la cadena.
-    mov al, '0' ; Reemplaza el '$' encontrado por un caracter nulo.
-    mov suma[si], al
-    inc si
-    cmp si, 2
-    jl LlenarCadena1
+        posicion 3, 0
+        mov ah, 09h
+        lea dx, Print_Nivel
+        int 21h
+        mov Max_Mostrar_Datos, 2
+        Call LlenarCadena
+        MOV AX, Nivel_actual      ; Cargar el valor de 'count' en AX
+        MOV DX, OFFSET Print_Dato_Actual
+        CALL INT_TO_STR   ; Llama a una funcion para convertir el numero en una cadena
+        posicion 3, 22
+        MOV AH, 09h        ; Servicio para imprimir una cadena
+        MOV DX, OFFSET  Print_Dato_Actual ; DX apunta a la cadena
+        INT 21h     
+        posicion 1, 40
+        mov ah, 09h
+        lea dx, Print_Puntos_Obtenidos
+        int 21h
+        mov Max_Mostrar_Datos, 5
+        Call LlenarCadena
+        
+        MOV AX, puntos_Obtenidos      ; Cargar el valor de 'count' en AX
+        MOV DX, OFFSET Print_Dato_Actual
+        CALL INT_TO_STR  ; Llama a una funcion para convertir el numero en una cadena
+        posicion 1, 60
+        MOV AH, 09h        ; Servicio para imprimir una cadena
+        MOV DX, OFFSET  Print_Dato_Actual ; DX apunta a la cadena
+        INT 21h 
+        
+    CALL Dibujar_Cuadro 
+    dec columna2
+    call Mostrar_Obstaculo
+    call Mostrar_Avatar
         RET
-    LlenarCadena ENDP
+        INT_TO_STR PROC
+        ; Esta funcion asume que el numero es menor o igual a 65535 (maximo valor de un registro AX)
+        PUSH AX
+        PUSH BX
+        PUSH CX
+        PUSH DX
+        
+        MOV CX, 10          ; Divisor para convertir en decimal
+        XOR BX, BX  
+        mov bx, Max_Mostrar_Datos        
+        add  bx, OFFSET Print_Dato_Actual  ; Puntero al final de la cadena
+        mov di, bx          ; DI apunta al final de la cadena
+        MOV BYTE PTR [DI], '$' ; Fin de cadena nul-terminada
+        XOR BX, BX 
+        ConvertLoop:
+            XOR DX, DX
+            DIV CX              ; Dividir AX por 10, resultado en AX, residuo en DX
+            ADD DL, '0'         ; Convertir el residuo en caracter
+            DEC DI
+            MOV [DI], DL       
+            INC BX
+            TEST AX, AX
+        JNZ ConvertLoop
+
+        LEA SI, [DI]        ; SI apunta al inicio de la cadena (justo después de '$')
+        MOV DI, OFFSET Print_Dato_Actual       ; DI apunta al destino de la cadena
+        MOV CX, BX          ; CX contiene la longitud de la cadena
+
+        CopyLoop:
+            LODSB               ; Cargar un byte desde SI
+            STOSB               ; Almacenar el byte en DI
+            LOOP CopyLoop
+
+        POP DX
+        POP CX
+        POP BX
+        POP AX
+
+        RET
+        INT_TO_STR ENDP
+
+        LlenarCadena proc near
+        mov si, 00h
+        LlenarCadena1:
+        mov al, '$'
+        repne scasb ; Busca el caracter '$' en la cadena.
+        mov al, '0' ; Reemplaza el '$' encontrado por un caracter nulo.
+        mov Print_Dato_Actual[si], al
+        inc si
+        cmp si, Max_Mostrar_Datos
+        jl LlenarCadena1
+            RET
+        LlenarCadena ENDP
+
     Mostrar_Datos ENDP
 
+    niveles_auto PROC
+        inc [puntos_Obtenidos]
+        mov [segundo_Se], 0
+        mov ah, 2ch
+        int 21H
+        mov segundo_Se, dh
+        mov al, primer_Segundo
+        cmp segundo_Se, al
+        jle es_menor
+        jmp es_mayor
+
+        es_menor:
+        mov bl, 60
+        add bl, segundo_Se
+        sub bl, primer_Segundo
+        mov resultado_resta, bl
+        cmp resultado_resta, 20
+        jl salir_niveles_auto
+        jge subir_nivel_auto
+        jmp salir_niveles_auto
+
+        es_mayor:
+        sub segundo_Se, al
+        cmp segundo_Se, 20
+        jl salir_niveles_auto
+        jge subir_nivel_auto
+        jmp salir_niveles_auto
+
+        subir_nivel_auto:
+        mov [primer_Segundo], 0
+        mov [segundo_Se], 0
+        mov [aux_primer_se], 0
+        mov [resultado_resta], 0
+        cmp [Nivel_actual], 15
+        jge seguir4 
+        call Actualizar_Nivel
+        seguir4:
+        mov ah, 2ch
+        int 21H
+        mov [primer_Segundo], dh
+        RET
+        salir_niveles_auto:
+        mov [segundo_Se], 0
+        RET
+    niveles_auto ENDP
     Iniciar ENDP
 Acerca PROC NEAR
     CALL Limpia
